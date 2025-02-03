bb6a8ad66113d6a53be5c84ea947862f0c2fd8ce
Carlota Cetina <carlotika26@gmail.com>
2025-02-03 01:36:54 +0100

    Added slider in design section

--- src/app/main/disenos/disenos.component.ts
+++ src/app/main/disenos/disenos.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-disenos',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './disenos.component.html',
   styleUrl: './disenos.component.scss'
 })
